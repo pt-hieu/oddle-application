d24cfc53ae11554cf3dc2eff9bf50378d52f1aef
pt-hieu <trunghieu.dev.2711@gmail.com>
2022-06-18 01:13:30 +0700

    feat: footer

---
+++ components/Footer.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import tw, { styled } from 'twin.macro';
+
+const FooterItemsData: {
+  icon: string;
+  label: string;
+  path: string;
+}[] = [
+  { icon: 'fa fa-search', label: 'Search', path: '/' },
+  { icon: 'fa fa-heart', label: 'Favorite', path: '/liked' },
+];
+
+const FooterItem = styled.a(({ active }: { active: boolean }) => [
+  tw`grid place-content-center cursor-pointer`,
+  active && tw`text-primary`,
+]);
+
+export default function Footer() {
+  const { pathname } = useRouter();
+
+  return (
+    <div className="h-[72px] grid grid-cols-2 py-2 shadow-[0_-4px_4px_0_#0000000D]">
+      {FooterItemsData.map(({ icon, label, path }) => (
+        <Link key={label} href={path} passHref>
+          <FooterItem active={path === pathname}>
+            <span className={`${icon} mx-auto leading-[29px]`} />
+            <div className="text-center text-xs">{label}</div>
+          </FooterItem>
+        </Link>
+      ))}
+    </div>
+  );
+}
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import { PropsWithChildren } from 'react';
 
+import Footer from './Footer';
+
 type TProps = {
   title: string;
   titleTemplate?: string;
@@ -20,8 +22,10 @@ export default function Layout({
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
-      <div className="w-[clamp(428px,466px,466px)] mobile:w-[428px] tablet:w-[clamp(428px,587px,587px)] mx-auto p-4">
-        {children}
+      <div className="w-[clamp(428px,466px,466px)] mobile:w-[428px] tablet:w-[clamp(428px,587px,587px)] mx-auto p-4 pb-0">
+        <div className="max-h-[calc(100vh-72px)] ">{children}</div>
+
+        <Footer />
       </div>
     </>
   );
